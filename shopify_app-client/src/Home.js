000616f93a90ad2ea5f681e361077b03d3162f84
Mely07 <damely.tineo07@gmail.com>
2021-05-06 22:21:19 -0400

    Set up update/edit functionality - allowing user/merchant to update product price.

--- shopify_app-client/src/Home.js
+++ shopify_app-client/src/Home.js
@@ -13,11 +13,20 @@ class Home extends Component {
             });
     }
 
-    onSubmit = (e) => {
+    onCreate = (e) => {
         e.preventDefault();
-        const form = new FormData(document.querySelector('form'));
-        fetch(`http://localhost:3000/products`, {
+        const form = new FormData(e.target);
+        fetch('http://localhost:3000/products', {
             method: "POST",
+            body: form
+        });
+    }
+
+    onUpdate = (e, id) => {
+        e.preventDefault();
+        const form = new FormData(e.target);
+        fetch('http://localhost:3000/products/' + id, {
+            method: "PATCH",
             body: form
         });
     }
@@ -25,25 +34,28 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <form id="form" name="form" onSubmit={this.onSubmit}>
-                    <label>Upload File</label>
-                    <input type="file" id="image" name="image" />
-                    <br />
-                    <label>Description</label>
-                    <input type="text" id="description" name="description" />
-                    <br />
-                    <label>Price</label>
-                    <input type="text" id="price" name="price" />
-                    <br />
-                    <input type="submit" />
-                </form>
                 <div>
+                    <form onSubmit={this.onCreate}>
+                        <label>Upload File</label>
+                        <input type="file" id="image" name="image" />
+                        <br />
+                        <label>Description</label>
+                        <input type="text" id="description" name="description" />
+                        <br />
+                        <label>Price</label>
+                        <input type="text" id="price" name="price" />
+                        <br />
+                        <input type="submit" />
+                    </form>
                     {this.state.products.map((product) => (
-                        <div key={product.id}>
+                        <form key={product.id} onSubmit={(e) => this.onUpdate(e, product.id)}>
                             <img src={product.image} alt="" />
                             <p> {product.description}</p>
-                            <p> {product.price}</p>
-                        </div>
+                            <label>Price</label>
+                            <input type="text" id="price" name="price" defaultValue={product.price} />
+                            <br />
+                            <input type="submit" />
+                        </form>
                     ))}
                 </div>
             </div>
